4137429902a0c3c738ba0e602234a1ae6afbf61d
md-moynul <mdmoynulislam58995@gmail.com>
2026-05-04 23:57:25 +0600

    feat(all-photos): add photo grid and card components

--- src/app/all-photos/page.jsx
+++ src/app/all-photos/page.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
-
-const AllPhotos = () => {
+import PhotoCard from '@/components/PhotoCard';
+import { getPhoto } from '@/lib/data';
+const AllPhotos = async () => {
+    const allPhotos = await getPhoto()
     return (
-        <div>
-            
+        <div className='container mx-auto py-5 '>
+            <h2 className='text-3xl font-bold'>Top Generations</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 py-5">
+                {allPhotos.map(photo => <PhotoCard key={photo.id} photo={photo} />)}
+
+            </div>
         </div>
     );
 };
--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -1,11 +1,12 @@
-import { Card, Link } from "@heroui/react";
+import { Card } from "@heroui/react";
 import Image from "next/image";
+import Link from "next/link";
 import { BiSolidDownload } from "react-icons/bi";
 import { FaHeart } from "react-icons/fa6";
 
 export default function PhotoCard({ photo }) {
     return (
-        <Card className=" border rounded-xl">
+        <Card className=" border rounded-xl space-y-2">
             <div className="relative w-full aspect-square">
                 <Image
                     src={photo.imageUrl}
@@ -17,24 +18,22 @@ export default function PhotoCard({ photo }) {
             </div>
 
             <Card.Header>
-                <Card.Title className="font-bold text-2xl">{photo.title}</Card.Title>
+                <Card.Title className="font-bold text-2xl text-nowrap">{photo.title}</Card.Title>
 
             </Card.Header>
             <div className="flex justify-between">
                 <div className="flex gap-2 items-center">
-                    <FaHeart size={20} className="text-pink-500"/> {photo.likes}
+                    <FaHeart size={20} className="text-pink-500" /> {photo.likes}
                 </div>
                 <div className="flex gap-2 items-center">
-                    <BiSolidDownload size={20} className="text-green-500"/> {photo.likes}
+                    <BiSolidDownload size={20} className="text-green-500" /> {photo.likes}
                 </div>
             </div>
             <Card.Footer>
                 <Link
-                    href="/"
-                    
+                    href={`/all-photos/${photo.id}`}
                 >
                     See Details
-                    <Link.Icon aria-hidden="true" />
                 </Link>
             </Card.Footer>
         </Card>
